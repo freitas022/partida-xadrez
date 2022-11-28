cdb66f00e4a44ca9d25b8f586aa4cff24f65a023
Matheus Freitas <mf022rj@gmail.com>
2022-11-28 16:19:43 -0300

    Camada de xadrez e impressão do tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,16 @@
 package application;
 
+import chess.ChessMatch;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Testando...");
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+		
+		
+
 	}
 
 }
